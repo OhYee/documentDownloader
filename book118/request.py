b95eaafac7c64039e89641a1d10427015d4aec4c
OhYee <oyohyee@oyohyee.com>
2020-05-25 19:20:56 +0800

    misc: reset proxies when is empty(#master)

--- book118/request.py
+++ book118/request.py
@@ -3,12 +3,19 @@ import requests
 session = requests.Session()
 http_proxy = ""
 https_proxy = ""
+proxies = {}
 
 
 def set_proxy(_http_proxy: str, _https_proxy: str):
-    global http_proxy, https_proxy
+    global http_proxy, https_proxy, proxies
     http_proxy = _http_proxy
     https_proxy = _https_proxy
+    proxies = {}
+    if http_proxy != '':
+        proxies['http'] = http_proxy
+    if https_proxy != '':
+        proxies['https'] = https_proxy
+    print("Proxy has been set", proxies)
 
 
 def get_page(url: str, params: object = None) -> requests.Response:
@@ -22,10 +29,8 @@ def get_page(url: str, params: object = None) -> requests.Response:
         params = {}
     while retry > 0:
         try:
-            response = session.get(url, params=params, timeout=10, proxies={
-                "http": http_proxy,
-                "https": https_proxy,
-            })
+            response = session.get(
+                url, params=params, timeout=10, proxies=proxies)
             break
         except Exception as e:
             print("Get page error: {}, {} times left...".format(e, retry))
