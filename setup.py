c538e31ebdd38086a808b19e78472ca4c8fdc702
OhYee <oyohyee@oyohyee.com>
2020-05-25 19:21:58 +0800

    chore: publish files(#master)

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+import setuptools
+import os
+
+
+version = os.popen("git tag -l").read().split("\n")[0]
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read().split("\n")
+
+opts = {
+    'name': 'documentDownloader',
+    'version': version,
+    'author': 'OhYee',
+    'author_email': 'oyohyee@oyohyee.com',
+    'url': 'https://github.com/OhYee/documentDownloader',
+    'description': u'book118文档下载器',
+    'long_description': long_description,
+    'long_description_content_type': "text/markdown",
+    'packages': setuptools.find_packages(),
+    'install_requires': requirements,
+    'entry_points': {
+        'console_scripts': [
+            'documentDownloader=book118:main',
+        ]
+    },
+    'classifiers': [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+}
+
+setup(**opts)
